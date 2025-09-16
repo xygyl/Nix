4e5b338263959b38c576acfddd29b579283d7509
xygyl <xygyl@proton.me>
2025-09-16 15:21:05 -0500

    misc

--- home/nexus-FW/modules/apps/yazi/plugins.nix
+++ home/nexus-FW/modules/apps/yazi/plugins.nix
@@ -2,14 +2,14 @@
 
 {
   programs.yazi.plugins = with pkgs; {
-    "chmod" = yaziPlugins.chmod;
-    "full-border" = yaziPlugins.full-border;
-    "git" = yaziPlugins.git;
-    "mount" = yaziPlugins.mount;
-    "smart-enter" = yaziPlugins.smart-enter;
+    "chmod"        = yaziPlugins.chmod;
+    "full-border"  = yaziPlugins.full-border;
+    "git"          = yaziPlugins.git;
+    "mount"        = yaziPlugins.mount;
+    "smart-enter"  = yaziPlugins.smart-enter;
     "smart-filter" = yaziPlugins.smart-filter;
-    "smart-paste" = yaziPlugins.smart-paste;
-    "toggle-pane" = yaziPlugins.toggle-pane;
-    "vcs-files" = yaziPlugins.vcs-files;
+    "smart-paste"  = yaziPlugins.smart-paste;
+    "toggle-pane"  = yaziPlugins.toggle-pane;
+    "vcs-files"    = yaziPlugins.vcs-files;
   };
 }
