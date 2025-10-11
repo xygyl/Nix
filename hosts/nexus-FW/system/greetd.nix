fa7307e1134fe7dde6c53397fef763f54cdf2902
xygyl <xygyl@proton.me>
2025-10-11 17:37:57 -0500

    remove unecessary greetd code

--- hosts/nexus-FW/system/greetd.nix
+++ hosts/nexus-FW/system/greetd.nix
@@ -7,9 +7,7 @@
 
     settings = {
       default_session = {
-        # command = "sh -c 'sleep 2 && exec dbus-run-session niri'";
         command = "tuigreet --cmd 'dbus-run-session niri'";
-        # command = "dbus-run-session niri";
         user = "${username}";
       };
     };
