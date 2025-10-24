019324a77c57241dfa2b475bf2fd5797b0b8f73d
xygyl <xygyl@proton.me>
2025-10-24 10:54:52 -0500

    shorten yazi settings.nix

--- home/nexus-FW/modules/apps/tui/yazi/settings.nix
+++ home/nexus-FW/modules/apps/tui/yazi/settings.nix
@@ -16,64 +16,28 @@
       wrap          = "yes";
     };
     open.prepend_rules = [
-    	{
-    	  name = "*.{gpg,asc}";
-    	  use = "decrypt";
-    	}
-    	{
-    	  name = "*.zst";
-    	  use = "extract_zstd";
-    	}
-    	{
-    	  name = "*.pdf";
-    	  use = "pdf";
-    	}
-    	{
-    	  name = "*.{png,jpg,gif,jfif,webp}";
-    	  use = "view";
-    	}
+    	{ name = "*.{gpg,asc}"; use = "decrypt"; }
+    	{ name = "*.zst"; use = "extract_zstd"; }
+    	{ name = "*.pdf"; use = "pdf"; }
+    	{ name = "*.{png,jpg,gif,jfif,webp}"; use = "view"; }
     ];
     opener = {
-      decrypt = [
-        { run = ''gpg -o "$${1%.*}" -d "$1"''; }
-      ];
-      edit = [
-        { run = ''hx "$@"''; block = true; }
-      ];
-      extract = [
-        { run = ''ouch d -y "$@"''; }
-      ];
-      extract_zstd = [
-        { run = ''zstd -d --long=31 "$@"''; }
-      ];
-      open = [
-        { run = ''xdg-open "$@"''; }
-      ];
-      play = [
-        { run = ''mpv --force-window=yes --loop "$@"''; orphan = true; }
-      ];
-      view = [
-        { run = ''nomacs -f "$@"''; }
-      ];
-      pdf = [
-        { run = ''firefox "$@"''; }
-      ];
+      decrypt = [ { run = ''gpg -o "$${1%.*}" -d "$1"''; } ];
+      edit = [ { run = ''hx "$@"''; block = true; } ];
+      extract = [ { run = ''ouch d -y "$@"''; } ];
+      extract_zstd = [ { run = ''zstd -d --long=31 "$@"''; } ];
+      open = [ { run = ''xdg-open "$@"''; } ];
+      play = [ { run = ''mpv --force-window=yes --loop "$@"''; orphan = true; } ];
+      view = [ { run = ''nomacs -f "$@"''; } ];
+      pdf = [ { run = ''firefox "$@"''; } ];
     };
     tasks = {
       image_alloc = 0;
       image_bound = [0 0];
     };
     plugin.prepend_fetchers = [
-    	{
-    	  id = "git";
-    	  name = "*";
-    	  run = "git";
-    	}
-    	{
-    	  id = "git";
-    	  name = "*/";
-    	  run = "git";
-    	}
+    	{ id = "git"; name = "*"; run = "git"; }
+    	{ id = "git"; name = "*/"; run = "git"; }
     ];
   };
 }
