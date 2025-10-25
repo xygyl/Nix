4997324e0138ca629cb78770c63341b36d0f996e
xygyl <xygyl@proton.me>
2025-10-25 11:16:44 -0500

    add games and code to package_conf.nix

--- home/nexus-FW/modules/apps/games/packages.nix
+++ home/nexus-FW/modules/apps/games/packages.nix
@@ -4,11 +4,14 @@
   home.packages = with pkgs; [
     # aisleriot
     # asciiquarium-transparent
+    # celestia
     chess-tui
+    # endless-sky
     # factorio-space-age
     # mindustry
     # openttd
     # osu-lazer
+    # pioneer
     # ponysay
     # quantframe
     # veloren
--- home/nexus-FW/package_conf.nix
+++ home/nexus-FW/package_conf.nix
@@ -2,13 +2,20 @@
   nixpkgs.config.permittedInsecurePackages = [
     "olm-3.2.16"
   ];
+
   nixpkgs.overlays = [
     (final: prev: {
       argtable = prev.argtable.overrideAttrs (old: {
         cmakeFlags = (old.cmakeFlags or []) ++ [
-          # "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+          "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
         ];
       });
+
+      # pioneer = prev.pioneer.overrideAttrs (old: {
+      #   cmakeFlags = (old.cmakeFlags or []) ++ [
+      #     "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
+      #   ];
+      # });
     })
   ];
 }
