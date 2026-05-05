782de8851a85f4ea5ce2b54aba19c42bb362a9c2
xygyl <xygyl@proton.me>
2026-05-04 23:30:56 -0500

    add xdg file drop to nexus

--- home/nexus/modules/apps/wm/niri/default.nix
+++ home/nexus/modules/apps/wm/niri/default.nix
@@ -1,5 +1,13 @@
+{pkgs, ...}:
+
 {
   imports = [
     ./niri.nix
   ];
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "*";
+  };
+
 }
