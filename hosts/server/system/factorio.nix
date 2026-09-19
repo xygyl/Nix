09a167b7983cd810f9acacf25d865052a9a8f4eb
xygyl <xygyl@proton.me>
2026-09-19 09:56:44 -0500

    update factorio on server to 2.1.19

--- hosts/server/system/factorio.nix
+++ hosts/server/system/factorio.nix
@@ -3,17 +3,17 @@
 {
   services.factorio = {
     enable = true;
-    # nixpkgs' versions.json hasn't caught up to 2.1.17 yet, so pin it
+    # nixpkgs' versions.json hasn't caught up to 2.1.19 yet, so pin it
     # directly. Remove this override once factorio-headless-experimental
-    # in nixpkgs reaches 2.1.17 or later.
+    # in nixpkgs reaches 2.1.19 or later.
     package = pkgs.factorio-headless-experimental.override {
       versions = {
         x86_64-linux.headless.experimental = {
-          name = "factorio_headless_x64-2.1.17.tar.xz";
-          version = "2.1.17";
+          name = "factorio_headless_x64-2.1.19.tar.xz";
+          version = "2.1.19";
           tarDirectory = "x64";
-          url = "https://factorio.com/get-download/2.1.17/headless/linux64";
-          sha256 = "02xha4pjlnbggxvmwvl3yx0qnijr4vfkf55rc1b8rcjw43mry590";
+          url = "https://factorio.com/get-download/2.1.19/headless/linux64";
+          sha256 = "1craxm64qlr5rqvp96addsk702inaw7c5nwn2xfyibk83pd0gdp6";
           needsAuth = false;
         };
       };
